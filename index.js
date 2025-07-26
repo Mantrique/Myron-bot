e57c76473bd5c1da68ed2471d890ce00cdaf48a7
Maksym Moskalenko <mantrique@gmail.com>
2025-07-26 18:56:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -121,8 +121,8 @@ async function openPosition(direction) {
   });
 
   const takeProfitPrice = direction === "long"
-    ? (price * 1.004).toFixed(2)
-    : (price * 0.996).toFixed(2);
+    ? (price * 1.01).toFixed(2)
+    : (price * 0.99).toFixed(2);
 
   await signedRequest("POST", "/fapi/v1/order", {
     symbol: SYMBOL,
